6a58e9c22d9dd8d414b06a0425d73004a4f75dbc
МГТУ ИУ7 <del880@bk.ru>
2016-11-13 16:01:41 +0300

    Add files via upload

---
+++ 2normal_short.asm
@@ -0,0 +1,35 @@
+;не эмулируется почти ничего, кроме метода и одного регистра R0 
+  $ADR 0000h; Автодекрементная		
+nc: 
+LXI SP,00b0h; 0b00 
+IN 20h;
+STA 0b00h;
+IN 25h;
+STA 0b01h
+POP B;  sp = 0b02h
+MVI A,B;
+CMA A;
+MVI B,A;
+MVI A,C;
+CMA A;
+MVI C,A;
+PUSH B;
+LXI SP,0faeh; 0afe
+end: JMP nc; 
+
+   $ADR 0000h; индексная
+LXI H,0b00h; 0b00 
+IN 20h;
+STA 0b00h;
+IN 25h;
+STA 0b01h
+MOV a,M; a=(HL)
+CMA A;
+MVI B,A;
+INX H;
+MOV A,M;
+CMA A;
+MVI C,A;
+LXI SP, 00b2h; 0b02
+PUSH B;
+end:; 
